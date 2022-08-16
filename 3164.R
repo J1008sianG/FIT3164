05c1ea1fb9e19c95f3eaa6344258f9a54090322c
J1008sianG <90944809+J1008sianG@users.noreply.github.com>
2022-08-16 10:27:46 +0800

    read articles from folder and append into df

--- 3164.R
+++ 3164.R
@@ -7,47 +7,54 @@ library("XML")
 library("xml2")
 library("RCurl")
 
-# read an article
-article <- read_xml("journal.pbio.0050304.xml")
 
-
-title <- xml_text(xml_find_all(article, xpath = "//title-group"))
-abstract <- xml_text(xml_find_all(article, xpath = "//abstract"))
-body <- xml_text(xml_find_all(article, xpath = "//body"))
-category <- xml_text(xml_find_all(article, xpath = "//article-categories"))
-
-clean <- function(x){
-  
-  x <-tolower(x)
-  
-  x <-removeWords(x,stopwords('en'))
-  
-  x <-removePunctuation(x)
-  
-  x <-removeNumbers(x)
-  
-  x <-stripWhitespace(x)
-  
-  x <-stemDocument(x)
-  
-  return(x) }
-
-
-title <- clean(title)
-abstract <- clean(abstract)
-body <- clean(body)
-category <- clean(category)
-
+file_list <- list.files()
 articles_table <- data.frame(matrix(ncol = 4, nrow = 0))
-articles_table <- rbind(articles_table, c(title,abstract,body,category))
 
-temp_abs <- ""
-if (length(abstract) > 1){
-   for (text in abstract){
-     temp_abs <- paste(temp_abs,text)
-   }
- }
-abstract <- temp_abs
+for (article in file_list) {
+  # read an article
+  article <- read_xml(article)
+  
+  
+  title <- xml_text(xml_find_all(article, xpath = "//title-group"))
+  abstract <- xml_text(xml_find_all(article, xpath = "//abstract"))
+  body <- xml_text(xml_find_all(article, xpath = "//body"))
+  category <- xml_text(xml_find_all(article, xpath = "//article-categories"))
+  
+  clean <- function(x){
+    
+    x <-tolower(x)
+    
+    x <-removeWords(x,stopwords('en'))
+    
+    x <-removePunctuation(x)
+    
+    x <-removeNumbers(x)
+    
+    x <-stripWhitespace(x)
+    
+    x <-stemDocument(x)
+    
+    return(x) }
+  
+  
+  title <- clean(title)
+  abstract <- clean(abstract)
+  body <- clean(body)
+  category <- clean(category)
+  
+  temp_abs <- ""
+  if (length(abstract) > 1){
+    for (text in abstract){
+      temp_abs <- paste(temp_abs,text)
+    }
+  }
+  abstract <- temp_abs
+
+  articles_table <- rbind(articles_table, c(title,abstract,body,category))
+
+
+}
 
 colnames(articles_table) = c("Title","Abstract","Paragraphs","Category")
 
