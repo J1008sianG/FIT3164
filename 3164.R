466e8b8474d02a873fc5bcd410b3eb32788c6b75
rongwen1 <110589641+rongwen1@users.noreply.github.com>
2022-09-08 00:38:25 +0800

    Update 3164.R

--- 3164.R
+++ 3164.R
@@ -220,3 +220,79 @@ nn.acc.2 <- sum(nn.cfm.2[1], nn.cfm.2[4]) / sum(nn.cfm.2[1:4])
 nn.acc <- (sum(nn.acc.1,nn.acc.2)/2)*100
 cat("Neural Network accuracy is: ", nn.acc, "%") 
 
+
+#####Visualization#####
+user_input_article = as.data.frame(test_data[2,])
+input_pred <- predict(naive.fit, user_input_article)
+
+#Output if predicted correctly or wrongly
+if(input_pred == user_input_article$Findability){   
+  print(paste("It has predicted Correctly, Findability = ", as.numeric(input_pred)))
+}else{
+  print(paste("It has predicted Wrongly, Findability = ", as.numeric(input_pred)))
+}
+
+#Create list for each placement
+input_title = (scan(text=user_input_article$Title, what="[[:space:]]"))
+input_abstract = as.list(scan(text=user_input_article$Abstract, what="[[:space:]]"))
+input_paragraph = as.list(scan(text=user_input_article$Paragraph, what="[[:space:]]"))
+input_category = as.list(scan(text=user_input_article$Category, what="[[:space:]]"))
+
+#Create new data frame to store Title words statistics
+input_df = as.data.frame((scan(text=user_input_article$Title, what="[[:space:]]")))
+names(input_df)[1] <- 'Title'
+input_df["Abstract"] = 0
+input_df["Paragraph"] = 0
+input_df["Category"] = 0
+input_df["Abstract_density"] = 0
+input_df["Paragraph_density"] = 0
+input_df["Category_density"] = 0
+input_df
+
+#Length for each placement
+abstract_total = length(input_abstract)
+paragraph_total = length(input_paragraph)
+category_total = length(input_category)
+
+#Calculate 
+for (i in 1:length(input_title)){
+  
+  #Abstract
+  abstract_count = 0
+  if (length(input_abstract) > 0){
+    for (j in 1:length(input_abstract)){
+      if (input_title[i] == input_abstract[j]){
+        abstract_count = abstract_count + 1
+      }
+    }
+  }
+  input_df[i, 2] = abstract_count
+  input_df[i, 5] = abstract_count / category_total
+  
+  #Paragraph
+  paragraph_count = 0
+  if (length(input_paragraph) > 0){
+    for (j in 1:length(input_paragraph)){
+      if (input_title[i] == input_paragraph[j]){
+        paragraph_count = paragraph_count + 1
+      }
+    }
+  }
+  input_df[i, 3] = paragraph_count
+  input_df[i, 6] = paragraph_count / paragraph_total
+  
+  #Category
+  category_count = 0
+  if (length(input_category) > 0){
+    for (j in 1:length(input_category)){
+      if (input_title[i] == input_category[j]){
+        category_count = category_count + 1
+      }
+    }
+  }
+  input_df[i, 4] = category_count
+  input_df[i, 7] = category_count / category_total
+  
+}
+
+input_df
